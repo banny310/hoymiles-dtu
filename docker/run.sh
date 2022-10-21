54f6416965e3656c6895640b37231bc947128eb7
Szymon Banaś <sb@impetbhp.pl>
2022-10-21 17:14:56 +0200

    - hotfix in start script

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/with-contenv bashio
 set +u
 
-CONFIG_PATH="/data/options.json"
+HA_CONFIG_PATH="/data/options.json"
+APP_CONFIG_PATH="/application.json"
+APP_JAR="/app.jar"
 
-if [ -e ${CONFIG_PATH} ]; then
+if [ -e ${HA_CONFIG_PATH} ]; then
   bashio::log.info "/data/options.json exists!"
 
   CONFIG_FORCE_mqtt_host=$(bashio::services mqtt "host")
@@ -19,19 +21,19 @@ if [ -e ${CONFIG_PATH} ]; then
   bashio::log.info "DTU: host=${CONFIG_FORCE_dtu_host}, port=${CONFIG_FORCE_dtu_port}"
 
   # shellcheck disable=SC2002
-  cat ${CONFIG_PATH} \
+  cat ${HA_CONFIG_PATH} \
     | jq --arg mqtt_host "$CONFIG_FORCE_mqtt_host" '.mqtt.host |= $mqtt_host' \
     | jq --arg mqtt_port "$CONFIG_FORCE_mqtt_port" '.mqtt.port |= $mqtt_port' \
     | jq --arg mqtt_username "$CONFIG_FORCE_mqtt_username" '.mqtt.username |= $mqtt_username' \
     | jq --arg mqtt_password "$CONFIG_FORCE_mqtt_password" '.mqtt.password |= $mqtt_password' \
-    > /application.json
+    > ${APP_CONFIG_PATH}
 
   bashio::log.info "banny310 - Home Assistant Hoymiles DTU Solar Data Gateway Add-on"
   bashio::log.info "Configuration:"
   cat /application.json
   bashio::log.info "Starting..."
 
-  java -Dconfig.override_with_env_vars=true -jar /app.jar
+  java -Dconfig.override_with_env_vars=true -Dconfig.file=${APP_CONFIG_PATH} -jar ${APP_JAR}
 else
   bashio::log.fatal "/data/options.json NOT exists!"
 fi
